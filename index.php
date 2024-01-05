3fff1c37a47e7b1d17bae55bc9767b462d875389
Bayu Prawira <bayu.bgpbali@gmail.com>
2024-01-05 10:37:56 +0800

    coba ignore files

--- index.php
+++ index.php
@@ -66,13 +66,13 @@
 switch (ENVIRONMENT)
 {
 	case 'development':
-		//error_reporting(-1);
-		//ini_set('display_errors', 1);
-		if($_SERVER["REMOTE_ADDR"]=='180.249.185.191'){
+		error_reporting(-1);
+		ini_set('display_errors', 1);
+		/*if($_SERVER["REMOTE_ADDR"]=='180.249.185.191'){
 			error_reporting(-1);
 			ini_set('display_errors', 1);
 		}
-		ini_set('display_errors', 0);
+		ini_set('display_errors', 0);*/
 	break;
 
 	case 'testing':
